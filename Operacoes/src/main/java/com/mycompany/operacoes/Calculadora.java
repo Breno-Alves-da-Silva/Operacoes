23c14231cdb786f1482318f99123d16004420388
Breno <brenosilva2015_alves@yahoo.com>
2023-12-06 21:22:40 -0300

    Pronto

---
+++ Operacoes/src/main/java/com/mycompany/operacoes/Calculadora.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.operacoes;
+
+/**
+ *
+ * @author Breno
+ */
+public class Operacoes {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
